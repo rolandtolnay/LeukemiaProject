440f1b0f4674e968f9b011dc9317f6450664d94f
dmu-23 <dmu-23@dmu-23s-Mac-mini.local>
2014-08-15 13:52:31 +0200

    Finalized the 3 navigation tabs together with proper name-refactoring

---
+++ Leukemia App/RTDiaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTDiaryViewController.h
+//  Leukemia App
+//
+//  Created by dmu-23 on 15/08/14.
+//  Copyright (c) 2014 dmu-23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTDiaryViewController : UIViewController
+
+@end
