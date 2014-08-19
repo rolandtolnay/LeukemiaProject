8456fd625bcd4296f392831f8756d884ce45b0ee
DMU-24 <dmu-24@macmini-b6d1f7.efif.dk>
2014-08-19 14:38:15 +0200

    Small changes to drawing part

---
+++ Leukemia App/RTSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTSettingsViewController.h
+//  Leukemia App
+//
+//  Created by DMU-24 on 19/08/14.
+//  Copyright (c) 2014 DMU-24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTSettingsViewController : UIViewController
+
+@end
