046b19ba6e749144d741f8de11b36a5289824ce5
DMU-24 <dmu-24@DMU-24s-Mac-mini.local>
2014-08-22 13:05:29 +0200

    Organized files in groups. Created DataManagement class.

---
+++ RTDataManagement.h
@@ -0,0 +1,13 @@
+//
+//  RTDataManagement.h
+//  Leukemia App
+//
+//  Created by DMU-24 on 22/08/14.
+//  Copyright (c) 2014 DMU-24. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RTDataManagement : NSObject
+
+@end
---
+++ RTDataManagement.m
@@ -0,0 +1,13 @@
+//
+//  RTDataManagement.m
+//  Leukemia App
+//
+//  Created by DMU-24 on 22/08/14.
+//  Copyright (c) 2014 DMU-24. All rights reserved.
+//
+
+#import "RTDataManagement.h"
+
+@implementation RTDataManagement
+
+@end
