98defc1bda14c6956cf6cf57f8d8b12befb1148d
nicolaiduus <dmu-24@DMU-24s-Mac-mini.local>
2014-09-11 11:11:53 +0200

    Continued the diary tab

--- Leukemia App/RTDiaryViewController.h
+++ Leukemia App/RTDiaryViewController.h
@@ -8,8 +8,17 @@
 
 #import <UIKit/UIKit.h>
 #import "VRGCalendarView.h"
+#import "NSDate+convenience.h"
+#import "RTDataManagement.h"
 
-@interface RTDiaryViewController : UIViewController <VRGCalendarViewDelegate>
+@interface RTDiaryViewController : UIViewController <VRGCalendarViewDelegate, UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UIView *calendarView;
+@property (weak, nonatomic) IBOutlet UILabel *dayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *weekDayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *monthLabel;
+@property (weak, nonatomic) IBOutlet UITableView *dataTableView;
+
+@property (strong, nonatomic) RTDataManagement *dataMangement;
+@property (strong, nonatomic) NSMutableArray *data;
 
 @end
