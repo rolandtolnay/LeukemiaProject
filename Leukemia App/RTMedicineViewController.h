2492197a42a284079842131d5fc2c75fa512f99c
nicolaiduus <dmu-24@DMU-24s-Mac-mini.local>
2014-09-26 12:13:00 +0200

    Modified medicine part.

--- Leukemia App/RTMedicineViewController.h
+++ Leukemia App/RTMedicineViewController.h
@@ -8,10 +8,28 @@
 
 #import <UIKit/UIKit.h>
 #import "LSWeekView.h"
+#import "NSDate+convenience.h"
 
 @interface RTMedicineViewController : UIViewController
 
-@property (strong,nonatomic) LSWeekView *weekSelector;
 @property (weak,nonatomic) IBOutlet UIView *weekSelectorView;
+@property (weak, nonatomic) IBOutlet UIView *bloodSampleView;
+@property (weak, nonatomic) IBOutlet UIView *medicineView;
+@property (weak, nonatomic) IBOutlet UILabel *noSampleLabel;
+@property (weak, nonatomic) IBOutlet UIButton *addSampleButton;
+@property (weak, nonatomic) IBOutlet UIButton *saveSampleButton;
+
+@property (weak, nonatomic) IBOutlet UITextField *hemoText;
+@property (weak, nonatomic) IBOutlet UITextField *thrombotext;
+@property (weak, nonatomic) IBOutlet UITextField *neutroText;
+@property (weak, nonatomic) IBOutlet UITextField *crptext;
+@property (weak, nonatomic) IBOutlet UITextField *natriumtext;
+
+@property (strong,nonatomic) LSWeekView *weekSelector;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *bloodSampleLabels;
+@property (strong, nonatomic) IBOutletCollection(UITextField) NSArray *bloodSampleTextFields;
+
+- (IBAction)addSample:(id)sender;
+- (IBAction)saveSample:(id)sender;
 
 @end
--- Leukemia App/RTMedicineViewController.m
+++ Leukemia App/RTMedicineViewController.m
@@ -20,21 +20,89 @@
     
     self.edgesForExtendedLayout = UIRectEdgeNone;
     
+    self.medicineView.layer.borderWidth = 1.0;
+    self.medicineView.layer.borderColor = [UIColor blackColor].CGColor;
+    
     self.weekSelector = [[LSWeekView alloc] initWithFrame:CGRectZero style:LSWeekViewStyleDefault];
     self.weekSelector.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     self.weekSelector.calendar = [NSCalendar currentCalendar];
+
     self.weekSelector.selectedDate = [NSDate date];
     
     __weak typeof(self) weakSelf = self;
     self.weekSelector.didChangeSelectedDateBlock = ^(NSDate *selectedDate)
     {
-        
+        [weakSelf updateNoSampleLabel];
+        [weakSelf removeBloodSampleUI];
     };
     
     [self.weekSelectorView addSubview:self.weekSelector];
     
 }
 
+-(void)updateNoSampleLabel{
+    NSLog(@"%@",self.weekSelector.selectedDate);
+    if(self.weekSelector.selectedDate.day != 10){
+        self.addSampleButton.hidden = NO;
+    self.noSampleLabel.text = @"There is no bloodsample for this date";
+    }
+    else{
+        self.noSampleLabel.text = @"Det er den 10. idag";
+        self.addSampleButton.hidden = YES;
+    }
+}
 
 
+- (IBAction)addSample:(id)sender {
+    [self addBloodSampleUI];
+}
+
+- (IBAction)saveSample:(id)sender {
+    [self removeBloodSampleUI];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [[event allTouches] anyObject];
+    if ([self.hemoText isFirstResponder] && [touch view] != self.hemoText) {
+        [self.hemoText resignFirstResponder];
+    }
+    else if ([self.thrombotext isFirstResponder] && [touch view] != self.thrombotext) {
+        [self.thrombotext resignFirstResponder];
+    }
+    else if ([self.neutroText isFirstResponder] && [touch view] != self.neutroText) {
+        [self.neutroText resignFirstResponder];
+    }
+    else if ([self.crptext isFirstResponder] && [touch view] != self.crptext) {
+        [self.crptext resignFirstResponder];
+    }
+    else if ([self.natriumtext isFirstResponder] && [touch view] != self.natriumtext) {
+        [self.natriumtext resignFirstResponder];
+    }
+    [super touchesBegan:touches withEvent:event];
+}
+
+-(void)addBloodSampleUI{
+    self.addSampleButton.hidden = YES;
+    self.noSampleLabel.text = @"";
+    for (UILabel *label in self.bloodSampleLabels) {
+        label.hidden = NO;
+    }
+    for(UITextField *txtField in self.bloodSampleTextFields){
+        txtField.hidden = NO;
+    }
+    self.saveSampleButton.hidden = NO;
+}
+
+-(void)removeBloodSampleUI{
+    for (UILabel *label in self.bloodSampleLabels) {
+        label.hidden = YES;
+    }
+    for(UITextField *txtField in self.bloodSampleTextFields){
+        txtField.hidden = YES;
+    }
+    self.saveSampleButton.hidden = YES;
+    self.addSampleButton.hidden = NO;
+    self.noSampleLabel.text = @"There is no bloodsample for this date";
+
+}
 @end
