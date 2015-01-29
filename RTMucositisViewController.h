549834b8353820df43465fdd1e52b0cd9534e89f
nicolaiduus <dmu-24@DMU-24s-Mac-mini.local>
2015-01-29 15:14:59 +0100

    Created mucositis controller

---
+++ RTMucositisViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTMucositisViewController.h
+//  Leukemia App
+//
+//  Created by DMU-24 on 29/01/15.
+//  Copyright (c) 2015 DMU-24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTMucositisViewController : UIViewController
+
+@end
